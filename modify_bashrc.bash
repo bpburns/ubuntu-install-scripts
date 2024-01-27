58dfcd4904725d9fcb5cb067dd7d2927a17d8638
Brian Barefoot Burns <3639671+bpburns@users.noreply.github.com>
2024-01-27 13:39:07 -0700

    Set HISTTIMEFORMAT and gnu screen alias in bashrc.

--- modify_bashrc.bash
+++ modify_bashrc.bash
@@ -1,4 +1,15 @@
+# Sets HISTSIZE to unlimited.
+# Sets HISTFILESIZE to unlimited.
+# Deletes all occurences of HISTTIME FORMAT.
+# Adds HISTTIMEFORMAT after HISTFILESIZE.
+# Removes any existing aliases for screen.
+# Sets screen alias.
+
 sed -i.bak \
     -e 's/^HISTSIZE.*/HISTSIZE=-1/' \
     -e 's/^HISTFILESIZE.*/HISTFILESIZE=-1/' \
+    -e '/^HISTTIMEFORMAT.\+/d' \
+    -e '/^HISTFILESIZE=-1/a HISTTIMEFORMAT="%a %d %b %Y %r %Z: "' \
+    -e '/^alias screen.\+/d' \
+    -e $'$a\\\n\\\nalias screen=\'screen -RaAD\'' \
 ~/.bashrc
